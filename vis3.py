a9d068c36e66076279782cc450ee2ab79a0b483d
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2024-05-13 16:03:59 +0200

    Update vis3.py

--- vis3.py
+++ vis3.py
@@ -2,7 +2,6 @@ import pygame
 import sys
 
 def reproducir_video(pantalla, ruta_video):
-    pygame.mixer.quit()  # Desactivar el mezclador de sonido predeterminado para evitar conflictos
     reproductor = pygame.movie.Movie(ruta_video)
     reproductor.set_display(pantalla, (0, 0, 1920, 1080))
     reproductor.play()
@@ -14,12 +13,20 @@ def main():
     pantalla2 = pygame.display.set_mode((1920, 1080))
     reproductor1 = reproducir_video(pantalla1, "/home/frank/Desktop/vis3/vis_3--en.mov")
     reproductor2 = reproducir_video(pantalla2, "/home/frank/Desktop/vis3/vis_3--pl.mov")
+    clock = pygame.time.Clock()
+    playing = True
 
-    while True:
+    while playing:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                playing = False
+
+        pygame.display.update()
+        clock.tick(30)
+
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     main()
+
